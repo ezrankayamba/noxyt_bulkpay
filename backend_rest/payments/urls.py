636ff07bffd1be1d5fe4a20da63f5dae71db98be
ezrankayamba <ezrankayamba@gmail.com>
2020-01-31 00:15:13 +0300

    Added manual entry

--- backend_rest/payments/urls.py
+++ backend_rest/payments/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('batches/', views.BatchListView.as_view()),
     path('batches/<int:pk>', views.BatchDetailView.as_view()),
-    # path('records/', views.PaymentListView.as_view()),
+    path('batches/manual-create/', views.ManualEntryCreateBatchView.as_view()),
     # path('records/create/', views.PaymentCreateView.as_view()),
 ]
--- backend_rest/payments/views.py
+++ backend_rest/payments/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics, permissions
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 from . import serializers
 from . import models
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class BatchListView(generics.ListCreateAPIView):
@@ -21,3 +23,21 @@ class BatchDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return models.Batch.objects.all()
+
+
+class ManualEntryCreateBatchView(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
+    required_scopes = ['payments', 'clients']
+
+    def post(self, request, format=None):
+        data = request.data
+        batch = models.Batch.objects.create(name=data['name'], comments=data['comments'])
+        records = data['records']
+        for r in records:
+            models.Payment.objects.create(account=r['account'], amount=r['amount'], reason=r['reason'], batch=batch)
+
+        return Response({
+            'status': 0,
+            'message': f'Successfully created {len(records)} records',
+            'batchId': 123
+        })
